29ecba62ebc380d7e48ae23b837a5d8f3ad18273
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-09-02 19:18:10 +0530

    Creating a gambling game using vanilla js

---
+++ project.js
@@ -0,0 +1,15 @@
+// 1. Despot some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. give the user their winnings
+// 7. play again.
+
+const prompt = require('prompt-sync')();
+
+function deposit() {
+  const depositAmount = prompt('Enter a deposit amount: ');
+}
+
+deposit();
